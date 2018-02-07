fb49de205c1f64ae3d3a9f1ddd7070917154298b
Marouane Berkak <marouane.168@gmail.com>
2018-02-07 09:29:49 +0000

    Delete fieldset

--- view/backend/myQuestionsView.php
+++ view/backend/myQuestionsView.php
@@ -11,80 +11,81 @@ $questionManager = new QuestionManager();
 $publishedQuestions = $questionManager->countPubQuestions($user);
 $unpublishedQuestions = $questionManager->countUnpubQuestions($user);
 ?>
-<fieldset>
-    <legend><h2>My published questions</h2></legend>
-    <?php
-    if($publishedQuestions == 0)
-    {
-        echo 'No questions';
-    }
-    else
-    {
-        ?>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th class="col-xs-9">Question</th>
-                <th class="col-xs-3">Action</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $req = $questionManager->readUserPubQuestions($user);
-            while($data = $req->fetch(PDO::FETCH_ASSOC))
-            {
-                ?>
-                <tr>
-                    <td class="col-xs-9"><?= htmlspecialchars($data['question']) ?></td>
-                    <td class="col-xs-3"><a href="index?pub=0&id=<?= $data['id'] ?>" class="btn btn-info">Unpublish</a>
-                        <a href="index?delete=1&id=<?= $data['id'] ?>" class="btn btn-danger">Delete</a></td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-        </table>
-        <?php
-    }
+<h2>My published questions</h2>
+<hr>
+<?php
+if($publishedQuestions == 0)
+{
+    echo 'No questions';
+}
+else
+{
     ?>
-</fieldset>
-    <fieldset>
-        <legend><h2>My unpublished questions</h2></legend>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th class="col-xs-9">Question</th>
+            <th class="col-xs-3">Action</th>
+        </tr>
+        </thead>
+        <tbody>
         <?php
-        if($unpublishedQuestions == 0)
-        {
-            echo 'No questions';
-        }
-        else
+        $req = $questionManager->readUserPubQuestions($user);
+        while($data = $req->fetch(PDO::FETCH_ASSOC))
         {
             ?>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th class="col-xs-9">Question</th>
-                    <th class="col-xs-3">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $req = $questionManager->readUserUnpubQuestions($user);
-                while($data = $req->fetch(PDO::FETCH_ASSOC))
-                {
-                    ?>
-                    <tr>
-                        <td class="col-xs-9"><?= htmlspecialchars($data['question']) ?></td>
-                        <td class="col-xs-3"><a href="index?pub=1&id=<?= $data['id'] ?>" class="btn btn-info">Publish</a>
-                            <a href="index?delete=1&id=<?= $data['id'] ?>" class="btn btn-danger">Delete</a></td>
-                    </tr>
-                    <?php
-                }
-                ?>
-                </tbody>
-            </table>
+            <tr>
+                <td class="col-xs-9"><?= htmlspecialchars($data['question']) ?></td>
+                <td class="col-xs-3"><a href="index?pub=0&id=<?= $data['id'] ?>" class="btn btn-info">Unpublish</a>
+                    <a href="index?delete=1&id=<?= $data['id'] ?>" class="btn btn-danger">Delete</a></td>
+            </tr>
             <?php
         }
         ?>
-    </fieldset>
+        </tbody>
+    </table>
+    <?php
+}
+?>
+
+<h2>My unpublished questions</h2>
+<hr>
+<?php
+if($unpublishedQuestions == 0)
+{
+    echo 'No questions';
+}
+else
+{
+    ?>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th class="col-xs-9">Question</th>
+            <th class="col-xs-3">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $req = $questionManager->readUserUnpubQuestions($user);
+        while($data = $req->fetch(PDO::FETCH_ASSOC))
+        {
+            ?>
+            <tr>
+                <td class="col-xs-9"><?= htmlspecialchars($data['question']) ?></td>
+                <td class="col-xs-3"><a href="index?pub=1&id=<?= $data['id'] ?>" class="btn btn-info">Publish</a>
+                    <a href="index?delete=1&id=<?= $data['id'] ?>" class="btn btn-danger">Delete</a></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+    <?php
+}
+?>
+
+
 <?php
 $content = ob_get_clean();
 require('view/templateBack.php');
